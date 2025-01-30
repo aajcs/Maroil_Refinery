0829522bd9f09819e1f45d9cf48d24778a53bfa7
aajcs <98472648+aajcs@users.noreply.github.com>
2025-01-30 10:34:13 -0400

    subiendo inicio de sesion con v4

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,55 @@
-import { handlers } from "@/auth"; // Referring to the auth.ts we just created
-export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { loginUser } from "../../userService";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        try {
+          console.log("credentials", credentials);
+          let user = null;
+
+          user = await loginUser(credentials);
+          console.log(user);
+
+          if (!user) {
+            // No user found, so this is their first attempt to login
+            // Optionally, this is also the place you could do a user registration
+            throw new Error("Datos invalidos.");
+          }
+
+          return user;
+        } catch (error) {
+          // console.log("errordeauth", error.response.data);
+          return null;
+        }
+        // return user object with their profile data
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user as any;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
